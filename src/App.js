bfa6eca1502da5593ed52ff5e14a0735eeb33074
Ade Maulana Akbar <maulanaakbarr90@gmail.com>
2023-10-06 15:37:21 +0700

    first commit for trial redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./redux/action";
 
-function App() {
+const App = () => {
+  const counter = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   );
-}
-
+};
 export default App;
